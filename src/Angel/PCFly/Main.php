29fb8a8dab047fe676297dc7584b23dcff5cee39
Angel <bubblicious712@gmail.com>
2018-01-27 23:53:11 -0500

    Removed class property $fly
    Removed class property $fly because it wastes time and PMMP has getAllowFlight()

--- src/Angel/PCFly/Main.php
+++ src/Angel/PCFly/Main.php
@@ -14,8 +14,6 @@ use pocketmine\command\CommandSender;
 use pocketmine\event\entity\EntityDamageEvent;
 
 class Main extends PluginBase implements Listener{
-
-    private $fly = [];
     
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
@@ -25,13 +23,11 @@ class Main extends PluginBase implements Listener{
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
         if(strtolower($command->getName()) == "fly"){
             if($sender->hasPermission("fly.command") || $sender->isOp()){
-                if(isset($this->fly[strtolower($sender->getName())])){
-                    unset($this->fly[strtolower($sender->getName())]);
+                if($sender->getAllowFlight() == true){
                     $sender->setAllowFlight(false);
                     $sender->setFlying(false);
                     $sender->sendMessage(TF::RED."Fly disabled!");
                 } else {
-                    $this->fly[strtolower($sender->getName())] = strtolower($sender->getName());
                     $sender->setAllowFlight(true);
                     $sender->setFlying(true);
                     $sender->sendMessage(TF::GREEN."Fly enabled!");
@@ -47,11 +43,10 @@ class Main extends PluginBase implements Listener{
         
         if(($p = $ev->getEntity()) instanceof Player){
             
-            if($ev->getCause() !== 4 && isset($this->fly[$p->getLowerCaseName()])){
+            if($ev->getCause() !== 4 && $p->getAllowFlight() == true){
                 $p->sendPopup(TF::RED . 'Fly disabled');
                 $p->setFlying(true);
                 $p->setAllowFlight(false);
-                unset($this->fly[$p->getLowerCaseName()]);
             }
         }
     }
