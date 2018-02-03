95228dd767c5ead0c55ce9b5f19970825e5c52ff
Angel <bubblicious712@gmail.com>
2018-02-03 00:01:03 -0500

    Added Config & Better way of doing stuff

--- src/Angel/PCFly/Main.php
+++ src/Angel/PCFly/Main.php
@@ -2,51 +2,94 @@
 
 namespace Angel\PCFly;
 
+
 use pocketmine\Player;
-use pocketmine\entity\Entity;
+
+use pocketmine\utils\Config;
+
 use pocketmine\event\Listener;
-use pocketmine\plugin\PluginBase;
-use pocketmine\utils\TextFormat as TF;
 
 use pocketmine\command\Command;
+
+use pocketmine\plugin\PluginBase;
+
 use pocketmine\command\CommandSender;
+
+use pocketmine\utils\TextFormat as TF;
 
 use pocketmine\event\entity\EntityDamageEvent;
 
+
+/**
+ * Class Main
+ * @package Angel\PCFly
+ */
 class Main extends PluginBase implements Listener{
+
+
+    /** @var Config */
+    private $cfg;
+    
     
     public function onEnable(){
+        $this->getLogger()->info(TF::GREEN . "PC Fly has been enabled!");
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->getLogger()->info("PC Fly made by Angel(@VortexZMcPe)");
+
+        $folder = $this->getDataFolder();
+        if(is_dir($folder) == false)
+            mkdir($folder);
+        
+        $this->cfg = is_file($folder . 'config.yml') ? new Config($folder . 'config.yml') : new Config($folder . 'config.yml', Config::YAML, [
+            'fly_command.true' => '&aFly enabled',
+            'fly_command.false' => '&cFly disabled!',
+            'fly_eventHit_disabled' => '&cNo Fly in PvP!',
+
+        ]);
     }
 
+    /**
+     * @param CommandSender $sender
+     * @param Command $command
+     * @param string $label
+     * @param array $args
+     * @return bool
+     */
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
-        if(strtolower($command->getName()) == "fly"){
-            if($sender->hasPermission("fly.command") || $sender->isOp()){
-                if($sender->getAllowFlight() == true){
-                    $sender->setAllowFlight(false);
-                    $sender->setFlying(false);
-                    $sender->sendMessage(TF::RED."Fly disabled!");
-                } else {
-                    $sender->setAllowFlight(true);
-                    $sender->setFlying(true);
-                    $sender->sendMessage(TF::GREEN."Fly enabled!");
-                }
-            } else {
-                $sender->sendMessage(TF::RED."You dont have permission to use this command");
+
+        if (strtolower($command->getName()) == 'fly'){
+
+            if ($sender instanceof Player == false){
+                $sender->sendMessage(TF::RED.'This game is only to be used in`game!');
+                return false;
             }
+
+            if ($sender->hasPermission('fly.command') == false){
+                $sender->sendMessage(TF::colorize($this->cfg->get('fly_noPermission')));
+                return false;
+            }
+
+            $sender->setAllowFlight($sender->getAllowFlight() == false ? true : false);
+            $table = [true => 'true', false => 'false'];
+            $sender->sendMessage(TF::colorize($this->cfg->get('fly_command.'.$table[$sender->getAllowFlight()])));
+            return true;
         }
         return true;
     }
 
-    public function onHit(EntityDamageEvent $ev){
-        
-        if(($p = $ev->getEntity()) instanceof Player){
-            
-            if($ev->getCause() !== 4 && $p->getAllowFlight() == true){
-                $p->sendPopup(TF::RED . 'Fly disabled');
-                $p->setFlying(true);
-                $p->setAllowFlight(false);
+    /**
+     * @param EntityDamageEvent $ev
+     */
+    public function onDamage(EntityDamageEvent $ev){
+        $entity = $ev->getEntity();
+
+        if ($ev->getCause() !== $ev::CAUSE_FALL){
+
+            if ($entity instanceof Player){
+
+                if ($entity->getAllowFlight() == true){
+                    $entity->setAllowFlight(false);
+                    $entity->sendMessage(TF::colorize($this->cfg->get('fly_eventHit_disabled')));
+                }
             }
         }
     }
