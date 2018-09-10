ac21e1ad67f2555bd8559d5985297443b333c32f
Angel <24781244+AngelsFlyyin@users.noreply.github.com>
2018-09-09 23:21:59 -0400

    Update Main.php

--- src/Angel/PCFly/Main.php
+++ src/Angel/PCFly/Main.php
@@ -54,7 +54,7 @@ class Main extends PluginBase implements Listener{
                 return false;
             }
 
-            if($sender->hasPermission("fly.command") || $sender->isOp() == false){
+            if(!$sender->hasPermission("fly.command") || !$sender->isOp()){
                 $sender->sendMessage(TextFormat::colorize($this->cfg->get("fly_noPermission")));
                 return false;
             }
